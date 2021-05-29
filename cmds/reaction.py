29bf771750627125cb79b57e38950289d34fc6bc
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-05-30 05:35:12 +0800

    Update reaction.py

--- cmds/reaction.py
+++ cmds/reaction.py
@@ -10,24 +10,21 @@ with open('setting.json', 'r', encoding='utf8') as jfile:
 
 class Reaction(Cog_Extension):
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self,payload): 
-      print(payload.emoji)
-      if payload.message_id == jdata['messageID']:                   #使用者新增反映的限定訊息
-          print("ok")
-          if str(payload.emoji) == jdata['trek_emoji']:              #判斷emoji
-            print("ok")
-            guild = self.bot.get_guild(payload.guild_id)            #取得當前所在伺服器          
+    async def on_raw_reaction_add(self,data): 
+      if data.message_id == jdata['messageID']:                   #使用者新增反映的限定訊息
+        if str(data.emoji) == jdata['trek_emoji']:              #判斷emoji
+            guild = self.bot.get_guild(data.guild_id)             #取得當前事件所在伺服器          
             role = guild.get_role(jdata['test_role'])                #取得伺服器內指定身分組
-            await payload.member.add_roles(role,reason="check-in")   #給予該成員身分組
-            await payload.member.send(f'你取得了 {role} 身分組')
+            await data.member.add_roles(role,reason="check-in")   #給予該成員身分組
+            await data.member.send(f'你取得了 {role} 身分組')
     @commands.Cog.listener()
-    async def on_raw_reaction_remove(self,payload):                    
-        if str(payload.emoji) == jdata['trek_emoji']:                #判斷emoji
-            guild = self.bot.get_guild(payload.guild_id)            #取得當前所在伺服器
-            user = guild.get_member(payload.user_id)                 #設定user變數
+    async def on_raw_reaction_remove(self,data):                    
+        if str(data.emoji) == jdata['trek_emoji']:                #判斷emoji
+            guild = self.bot.get_guild(data.guild_id)             #取得當前事件所在伺服器
+            user = guild.get_member(data.user_id)                 #設定user變數
             role = guild.get_role(jdata['test_role'])                #取得伺服器內指定身分組
             await user.remove_roles(role)                            #移除該成員身分組
-            await payload.user.send(f'你被移除了 {role} 身分組')
+            await user.send(f'你被移除了 {role} 身分組')
            
 def setup(bot):
   bot.add_cog(Reaction(bot))
