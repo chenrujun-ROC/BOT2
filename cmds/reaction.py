a3119bed82852ab4ddecad7fc4b74c20933f8cef
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-06-14 14:03:16 +0800

    Update reaction.py

--- cmds/reaction.py
+++ cmds/reaction.py
@@ -14,7 +14,7 @@ class Reaction(Cog_Extension):
       if payload.message_id == jdata['messageID']:                   #使用者新增反映的限定訊息
         if str(payload.emoji) == jdata['trek_emoji']:                #判斷emoji
             guild = self.bot.get_guild(payload.guild_id)             #取得當前事件所在伺服器          
-            role = guild.get_role(jdata['test_role'])                #取得伺服器內指定身分組
+            role = guild.get_role(jdata['role'])                #取得伺服器內指定身分組
             await payload.member.add_roles(role)                     #給予該成員身分組
             await payload.member.send(f'你取得了 {role} 身分組')      #pm成員已被加入'test_role'身分組
     @commands.Cog.listener()
@@ -25,7 +25,8 @@ class Reaction(Cog_Extension):
             role = guild.get_role(jdata['test_role'])                #取得伺服器內指定身分組
             await user.remove_roles(role)                            #移除該成員身分組                        
             await user.send(f'你被移除了 {role} 身分組')              #pm成員已被從'test_role'身分組移除
-           
+    
+    
 def setup(bot):
   bot.add_cog(Reaction(bot))
 
