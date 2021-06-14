38d3d3b7a14c9a8bc8f555d5c6a730d6cd3110cd
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-06-14 14:03:20 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -2,12 +2,22 @@ import discord
 from discord.ext import commands
 from core.classes import Cog_Extension, Gloable_Data
 from core.errors import Errors
-import json, datetime, asyncio
+import json, datetime, asyncio, random
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
    jdata = json.load(jfile)
 
 class Event(Cog_Extension):
+
+  @commands.Cog.listener()
+  async def on_member_join(self, member):
+	  channel = self.bot.get_channel(int(jdata['welcome_channel']))
+	  await channel.send(f'{member}  進入!(*´∀`)~♥')
+  @commands.Cog.listener()
+  async def on_member_remove(self, member):
+	  channel = self.bot.get_channel(int(jdata['leave_channel']))
+	  await channel.send(f'{member}  離開!ฅ(๑*д*๑)ฅ!!')
+
   @commands.Cog.listener()
   async def on_command_error(self, ctx, error):
     '''指令錯誤觸發事件'''
@@ -21,13 +31,14 @@ class Event(Cog_Extension):
       await Errors.default_error(self, ctx, error)
 
   @commands.Cog.listener()
-  async def on_member_join(self, member):
-	  channel = self.bot.get_channel(int(jdata['welcome_channel']))
-	  await channel.send(f'{member}入侵!(´⊙ω⊙`)')
-  @commands.Cog.listener()
-  async def on_member_remove(self, member):
-	  channel = self.bot.get_channel(int(jdata['leave_channel']))
-	  await channel.send(f'{member}離開!(´⊙ω⊙`)')
+  async def on_message(self, msg):
+    channel = msg.channel.id
+    if channel == int(jdata['閒聊區_channel']):
+      if msg.content in jdata['keyword'] and msg.author != self.bot.user:
+        random_pic = random.choice(jdata['hello_pic'])
+        await msg.channel.send(random_pic)
+        
+
 
 def setup(bot):
   bot.add_cog(Event(bot))
